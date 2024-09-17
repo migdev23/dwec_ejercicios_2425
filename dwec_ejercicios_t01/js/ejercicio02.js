65d269f325de0e806794074923110e47d942fa3e
A. Montoro <amontoro.sistemasinformaticos@gmail.com>
2024-09-17 23:04:36 +0200

    Revisión ejercicio 02

--- dwec_ejercicios_t01/js/ejercicio02.js
+++ dwec_ejercicios_t01/js/ejercicio02.js
@@ -1,29 +1,50 @@
-const pedirYear = () =>{
-  let year = parseInt(prompt("Introduce el año para comprobar si es bisiesto"))
+//TODO Cerrar con ;
+//TODO español o inglés
+const pedirYear = () => {
+  let year = parseInt(prompt("Introduce el año para comprobar si es bisiesto"));
   
-  if(year < 0 || year > new Date().getFullYear()){
-    return null
+  // if (isNaN(year) || year < 0 || year > new Date().getFullYear()) {
+  if(year < 0 || year > new Date().getFullYear()) {
+    return null;
   }
-
-  return year
-
+  return year;
 }
 
-const comprobarBisiesto = (year) =>{
-  if( ((year%4 == 0) && (year%100 != 0)) || (year%400 == 0)){
-    alert("Bisiesto")
+// ! espacios entre operadores
+const comprobarBisiesto = (year) => {
+  if( ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0) ){
+    //alert("Bisiesto");
+    return true;
   }else{
-    alert("No Bisiesto")
+    //alert("No Bisiesto");
+    return false;
   }
 }
 
-let year = pedirYear()
-comprobarBisiesto()
 /*
-if(year !== null){
-  if(comprobarBisiesto()){
+const comprobarBisiesto = (year) => {
+  return ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0));
+}
+*/
+
+let year = pedirYear();
+// ! Parámetro de entrada --> comprobarBisiesto()
+
+// TODO 
+if(year !== null) {
+  // ! Parámetro de entrada
+  // ! Conflicto entre parámetros devueltos por la función.
+  if(comprobarBisiesto(year)){
     alert("Bisiesto")
   }else{
     alert("No Bisiesto")
   }
-}*/
+} else {
+  alert("El año introducido no es válido.");
+}
+
+/** TEST: 
+ * 2000: SÍ (divisible por 100 y por 400)
+ * 1900: NO
+ * 2024: SÍ
+*/
