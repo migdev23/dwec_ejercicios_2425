a80c99c3f91f6cfc46b51823b2b984bdcaefdd2b
Miguel Rodriguez <miguelrodriguezdevv@gmail.com>
2024-09-17 13:03:47 +0200

    Ejercicio1

---
+++ dwec_ejercicios_t01/js/ejercicio01.js
@@ -0,0 +1,30 @@
+const sacarNota = (media) => {
+  if (media >= 0 && media < 5) {
+    return console.log("Suspenso");
+  }
+
+  if (media >= 5 && media < 7) {
+    return console.log("APROBADO");
+  }
+
+  if (media >= 7 && media < 8.5) {
+    return console.log("NOTABLE");
+  }
+
+  if (media >= 8.5 && media <= 10) {
+    return console.log("SOBRESALIENTE");
+  }
+
+  return console.log("A ocurrido un error");
+};
+
+let sumaNumerosPromt = 0;
+let media = 0;
+
+for (let index = 1; index < 4; index++) {
+  sumaNumerosPromt += parseFloat(prompt(`Introduce tu nota ${index}`));
+}
+
+media = sumaNumerosPromt / 3;
+
+sacarNota(media);
