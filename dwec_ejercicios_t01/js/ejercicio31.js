285b7c351871fc289e45159ec0707cb0ea7388a6
Miguel Rodriguez <miguelrodriguezdevv@gmail.com>
2024-10-24 10:21:19 +0200

    ejer 31 v3

--- dwec_ejercicios_t01/js/ejercicio31.js
+++ dwec_ejercicios_t01/js/ejercicio31.js
@@ -3,16 +3,17 @@ const validaDNIyCIF = (cadena) => {
     const patronDNI = /^(\d{8})([A-Z])$/;
     
     const patronCIF = /^([ABCDEFGHJKLMNPQRSUVW])(\d{7})([0-9A-JXP])$/;
-    
-    const dniLetrasArr = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
 
-    let statusReturn = false
+    const dniLetrasArr = ['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    const lastLetter = cadena.charAt(cadena.length-1).toUpperCase();
+
+    let statusReturn = false;
        
 
-    if(patronDNI.test(cadena) && dniLetrasArr.includes(cadena.charAt(cadena.length-1))){
-        const numberDni = cadena.split(cadena.charAt(cadena.length-1))[0];
+    if(patronDNI.test(cadena) && dniLetrasArr.includes(lastLetter)){
+        const numberDni = cadena.split(lastLetter)[0];
         
-        const letterDni = cadena.charAt(cadena.length-1);
+        const letterDni = lastLetter;
         
         const rest = (numberDni%23) > (dniLetrasArr.length-1) ? null : numberDni%23;
         
@@ -27,7 +28,7 @@ const validaDNIyCIF = (cadena) => {
 
     }
     else if(patronCIF.test(cadena)){
-        
+    
         const cif_no_first_last_caracter = cadena.slice(1, cadena.length-1);
         const sumPair = parseInt(cif_no_first_last_caracter[2-1]) + parseInt(cif_no_first_last_caracter[4-1]) + parseInt(cif_no_first_last_caracter[6-1])
         const evenIndex = [1-1,3-1,5-1,7-1];
@@ -43,20 +44,16 @@ const validaDNIyCIF = (cadena) => {
         const sumFinal = sumEven + sumPair;
         const codeControl = (sumFinal + "").length == 2 ? (10 - parseInt((sumFinal + "")[1])) : (10-parseInt((sumFinal + "")[0]));
 
-        if(isNaN(cadena.charAt(cadena.length-1)) && cadena.charAt(cadena.length-1) != 'X' && cadena.charAt(cadena.length-1) != 'P'){
-            console.log(letrasAJ[codeControl-1], cadena.charAt(cadena.length-1))
-            if(letrasAJ[codeControl-1] == cadena.charAt(cadena.length-1)){
-                statusReturn = true
-            };
-        }else if(!isNaN(cadena.charAt(cadena.length-1))){
-
-            if(parseInt(cadena.charAt(cadena.length-1)) == codeControl) {statusReturn = true}
-            
-        }else{
-            console.log('Hacer xp')
+        if(isNaN(lastLetter) && lastLetter != 'X' && lastLetter != 'P' && (letrasAJ[codeControl-1] == lastLetter)){
+             statusReturn = true;
+        }else if(!isNaN(lastLetter) && (parseInt(lastLetter) == codeControl)){
+            statusReturn = true;
+        }else if((codeControl+64) == 88 || (codeControl+64) == 80){
+            statusReturn = true;
         }
+        
+    }
 
-    } 
     return statusReturn
 }
 
