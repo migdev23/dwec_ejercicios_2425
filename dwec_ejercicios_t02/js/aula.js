c5533fb2ee9eec72a3984d188cfc12aa1f429b24
Miguel <miguelrodriguezdevv@gmail.com>
2024-11-05 12:46:12 +0100

    ejercicio2 v1

--- dwec_ejercicios_t02/js/aula.js
+++ dwec_ejercicios_t02/js/aula.js
@@ -1,21 +1,39 @@
 function Aula(numeroAlumnosAula, identificadorAula, descripcionAula) {
+    
     this._numeroAlumnosAula = numeroAlumnosAula;
+    
     this._identificadorAula = identificadorAula;
+    
     this._descripcionAula = descripcionAula;
-    this._arrayAlumnos = []
+    
+    this._arrayAlumnos = [];
 
-    this.pedirDatosUnAlumno = () => {
-        validar(datos)
+    this.validarNota = (nota) => {
+        if(isNaN(nota) || nota < 0 || nota > 10) return false;
+        return true;
+    }
+
+    this.pedirNotaPrompt = (trimestre) => {
+        let nota = "";
+        do 
+            nota = prompt(`Introduce la nota del Trimestre: ${trimestre}, (0 al 10)`);
+        while (!this.validarNota(nota));
+    }
+
+    this.pedirDatosUnAlumno = () => {       
+        
         //TODO: HACER VALIDACIONES Y BUCLE
+        
         const nombre = prompt('Introduce el nombre del alumno:');
         const dni = prompt('Introduce el dni del alumno');
         const fechaNac = prompt('Introduce la fecha de nacimiento:');
-        const notaT1 = prompt('Introduce la nota del Trimestre 1:');
-        const notaT2 = prompt('Introduce la nota del Trimestre 2:');
-        const notaT3 = prompt('Introduce la nota del Trimestre 3:');
+        const notaT1 = this.pedirNotaPrompt(1);
+        const notaT2 = this.pedirNotaPrompt(2);
+        const notaT3 = this.pedirNotaPrompt(3);
         const sexo = prompt('Introduce el sexo del alumno (h,m,o): ');
 
         return new Alumno(nombre, fechaNac, dni, notaT1, notaT2, notaT3, sexo);
+
     };
 
     this.pedirDatos = () => {
